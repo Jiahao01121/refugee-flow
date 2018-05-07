e4fcc9dab8e637abb2b4b6f2da90cdf9970cda6c
Abin Abraham <abin.abraham4@gmail.com>
2018-05-07 17:24:21 -0400

    start work on landing page

---
+++ src/App/components/Landing.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import LandingPage from '../stylesheets/LandingPage.css'
+
+export default class Landing extends Component {
+  render() {
+    return(
+      <div className='image mid'>
+        <h2>Lorem ipsum dolor sit amet, ei ullum postea delenit mei. In debet
+          forensibus vim, liber putant dissentiet sit in. Ridens propriae democritum
+          qui ne, definiebas disputando at vix, his suas discere recteque et. Cu sit
+          choro salutatus deseruisse. Has soleat apeirian definiebas an, ex qui
+          clita postulant.</h2>
+      </div>
+    )
+  }
+}
