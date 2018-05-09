e64726ab06553ba8a1c1049e06cb4b932458f379
will_su <jiahao01121@gmail.com>
2018-05-09 19:35:03 -0400

    first version done!!!!!

--- src/App/components/Landing.js
+++ src/App/components/Landing.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  height: ${window.innerHeight - 60 + 'px'};
+`
 const Video = styled.video`
   position: fixed;
   right: 0;
@@ -9,34 +14,48 @@ const Video = styled.video`
   min-height: 100%;
   width: auto;
   height: auto;
-  z-index: -100;
+`
+const Img = styled.img`
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+  width: auto;
+  height: auto;
+  opacity: 0.3;
+  filter: brightness(0.3);
+  z-index: -1;
 `
 
-const Quote = styled.h1`
+const Quote = styled.p`
   position: absolute;
-  font-family: 'Roboto', sans-serif;
-  top: 30%;
-  left: 25%;
-  width: 50%;
-  font-size: 20px;
-  letter-spacing: 3px;
+  font-family: 'Roboto';
+  font-weight: 100;
+  font-size: 40px;
+  line-height: 1.4;
   color: #fff;
-  font-family: Oswald, sans-serif;
   text-align: center;
-  z-index: -99;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+  width: 65%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-70%);
+  z-index: 1;
+
+  &>strong{
+    color: #41EDB8;
+    font-weight: 400;
+  }
 `
 
 const QuoteBy = styled.span`
-  font-family: 'Roboto', sans-serif;
-  letter-spacing: 0;
-  font-weight: 300;
+  font-family: 'Miller-DisplaySC';
   font-size: 16px;
   line-height: 50px;
   display: flex;
   justify-content: flex-end;
+  top: 30px;
+  position: relative;
 `
 
 const SideLinks = styled.ul`
@@ -50,11 +69,10 @@ const SideLinks = styled.ul`
   position: fixed;
   width: 100vh;
   bottom: 0;
-  right: 2.5em;
+  right: 4.5em;
   line-height: 2em;
   transform: rotate(90deg);
   transform-origin: 100% 100%;
-  z-index: -99;
 `
 
 const List = styled.li`
@@ -64,32 +82,57 @@ const List = styled.li`
 `
 
 const Link = styled.a`
+  font-family: 'Roboto';
+  font-weight: 400;
+  font-size: 12px;
   text-decoration: none;
   color: white;
+  z-index: 1;
+`
+
+const LaunchViz = styled.a`
+  width: 190px;
+  padding: 20px 20px;
+  position: absolute;
+  top: ${window.innerHeight -60-80 - 30 + 'px'};
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: center;
+  font-family: 'Roboto';
+  font-size: 15px;
+  color: white;
+  text-decoration: none;
+  border-radius: 2px;
+  border: 2px solid white;
+  transition: all 400ms;
+  z-index: 1;
+
+  &:hover{
+    cursor: pointer;
+    background: #0C0C14;
+    border-color: #352554;
+  }
 `
 
 export default class Landing extends Component {
   render() {
     return(
-      <div>
-        <Quote>"The world is in much greater disarray than it was during the Cold War, in the Cold War,
-          the world had two fundamental concentrations of power in the United States and the Soviet Union,
-          and both respective alliance-systems. We understood how to avoid direct confrontation,
-          because anything might lead to nuclear escalation, and we have formal or informal roles. Well,
-          now is anything but. We have power much more widely distibuted in the world, plus we have globalization."
-          <QuoteBy> -- Richard Haass, Vice Interview, Oct 2017</QuoteBy>
+      <Wrapper>
+        <Quote>"The world is in much <strong>greater disarray</strong> than it was during the Cold War, in the Cold War, the world had two fundamental concentrations of power in the United States and the Soviet Union, and both respective alliance-systems. We understood how to <strong>avoid direct confrontation</strong>, because anything might lead to nuclear escalation, and we have formal or informal roles. <strong> Well, now is anything but</strong>. We have power much more widely distibuted in the world, plus we have globalization." <QuoteBy> - richard haass, vice interview, oct 2017</QuoteBy>
         </Quote>
         <div>
           <SideLinks>
-            <List><Link href="https://www.behance.com" target="_blank">Behance</Link></List>
-            <List><Link href="https://www.behance.com" target="_blank">Behance</Link></List>
-            <List><Link href="https://www.github.com" target="_blank">Github</Link></List>
+            <List><Link href="https://www.behance.net/will-su" target="_blank">Behance</Link></List>
+            <List><Link href="https://github.com/Jiahao01121/refugee-flow" target="_blank">Github</Link></List>
           </SideLinks>
         </div>
-        <Video autoPlay loop muted plays-inline>
+        {/* <Video autoPlay loop muted plays-inline>
           <source src="https://player.vimeo.com/external/158148793.hd.mp4?s=8e8741dbee251d5c35a759718d4b0976fbf38b6f&profile_id=119&oauth2_token_id=57447761" type="video/mp4" />
-        </Video>
-      </div>
+        </Video> */}
+        <Img src="https://www.activistpost.com/wp-content/uploads/2015/09/turkish-soldiers-stand-guard-syrian-refugees-wait-behind-border-fences.jpg"></Img>
+
+        <LaunchViz href='/war'>Launch Visualization</LaunchViz>
+      </Wrapper>
     )
   }
 }
--- src/App/components/RefugeeRoute.js
+++ src/App/components/RefugeeRoute.js
@@ -1,12 +1,55 @@
 import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  height: ${window.innerHeight - 60 + 'px'};
+
+`
+
+const Img = styled.img`
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+  width: auto;
+  height: auto;
+  opacity: 0.3;
+  filter: brightness(0.3);
+  z-index: -1;
+`
+
+const UnderDev = styled.p`
+  position: absolute;
+  font-family: 'Roboto';
+  font-weight: 100;
+  font-size: 40px;
+  line-height: 1.4;
+  color: #fff;
+  text-align: center;
+  width: 65%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-100%);
+  z-index: 1;
+
+  &>strong{
+    color: #41EDB8;
+    font-weight: 400;
+  }
+`
+
 
 export default class RefugeeRoute extends Component {
   render() {
     return(
-      <div>
-        <h3>Refugee Route</h3>
-      </div>
+      <Wrapper>
+        <UnderDev>Currently Under Construction<br/> Stay Tuned...</UnderDev>
+        <Img src="https://static01.nyt.com/images/2016/04/18/blogs/02australialetter32-18-lens-refugees-slide-8AS8-copy/18-lens-refugees-slide-8AS8-superJumbo.jpg?quality=90&auto=webp"></Img>
+      </Wrapper>
     )
   }
 }
