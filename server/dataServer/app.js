da762564a15832a0a2c9f8238d250fe3b86b21c6
will_su <jiahao01121@gmail.com>
2018-05-13 16:49:50 -0400

    adding server support

--- server/dataServer/app.js
+++ server/dataServer/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var app = express();
 var path = require('path');
 
 // var favicon = require('serve-favicon');
@@ -10,7 +11,6 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var dataRoutes = require('./routes/dataRoute');
 
-var app = express();
 
 // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
@@ -18,24 +18,23 @@ var app = express();
 
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://will:will@ds145118.mlab.com:45118/refugee-flow');
+mongoose.connect('mongodb://production:production@ds145118.mlab.com:45118/refugee-flow');
 
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(function(req, res, next) {
+app.use('/static',express.static(path.join(__dirname, '/build/static/')));
+app.use('/assets',express.static(path.join(__dirname, '/build/assets/')));
+app.use('*',function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 app.use('/', index);
+app.use('/war', index);
+app.use('/route', index);
 app.use('/data',dataRoutes)
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+app.use(function(req, res, next) {next(404)});
 
 // error handler
 // app.use(function(err, req, res, next) {
